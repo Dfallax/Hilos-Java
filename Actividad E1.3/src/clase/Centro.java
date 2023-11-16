33387b5e128591707184d28ee3b556c4ac99cfb3
Dfallax <dfalla0001@gmail.com>
2023-11-16 14:18:15 +0100

    actividad Inicio.

---
+++ Actividad E1.3/src/clase/Centro.java
@@ -0,0 +1,11 @@
+package clase;
+
+public class Centro {
+
+	private String nombre;
+	private String especialidad;
+	//private Doctor doctor;
+	private String fecha;
+	private String hora;
+	
+}
---
+++ Actividad E1.3/src/clase/Paciente.java
@@ -0,0 +1,16 @@
+package clase;
+
+public class Paciente {
+
+	public static final int MAA_CARACTER = 9;
+	private String id;
+	private String nombre;
+	private String Apellido_1;
+	private String Apellido_2;
+	private String nacimineto;
+	private String localidad;
+	
+	
+	
+	
+}
